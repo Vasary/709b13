03f9486da99fd4102a363a24ffe9daa59a4d15f0
Гиевой Виктор <gievoi.v@gmail.com>
2019-01-16 18:43:10 +0300

    Keys mount

--- docker/mailer/entrypoint.sh
+++ docker/mailer/entrypoint.sh
@@ -5,5 +5,4 @@ service dovecot start
 service rsyslog start
 service opendkim start
 
-while ! tail -f /var/log/mail.log ; do echo "Waiting for log file"; sleep 1 ; done
-tail -f /var/log/mail.log
+while ! tail -fn50 /var/log/mail.log ; do echo "Waiting for log file"; sleep 1 ; done
