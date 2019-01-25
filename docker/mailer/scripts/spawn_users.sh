af66a7848502d511a755b2149cc066df7ed100cb
Vasary <gievoi.v@gmail.com>
2019-01-26 00:16:39 +0300

    virtual aliases

--- docker/mailer/scripts/spawn_users.sh
+++ docker/mailer/scripts/spawn_users.sh
@@ -17,8 +17,9 @@ NC='\033[0m'
 
 echo -e "\nManage users:"
 
-echo -e "\nFlushing aliases map"
+echo -e "\nFlushing aliases/virtual maps"
 cat /dev/null > /etc/postfix/aliases
+cat /dev/null > /etc/postfix/virtual
 
 cat /etc/users | while read line; do
     echo -e "\n"
@@ -27,6 +28,7 @@ cat /etc/users | while read line; do
     name=${userData[0]}
     password=${userData[1]}
     alias="${userData[2]} ${name}@${DOMAIN}"
+    virtual="${userData[2]} ${name}"
 
     if ! id -u ${name} > /dev/null 2>&1; then
         if useradd -m -d /home/${name} -s /bin/false -p ${password} ${name}; then
@@ -40,12 +42,23 @@ cat /etc/users | while read line; do
     printf "${NC}[ ${GREEN}ok ${NC}] Updating ${name} password. Use this: ${password}\n"
 
     if echo ${alias} >> /etc/postfix/aliases; then
-        printf "${NC}[ ${GREEN}ok ${NC}] Updating alias for ${name} to {$alias}\n"
+        printf "${NC}[ ${GREEN}ok ${NC}] Updating alias for ${name} to ${alias}\n"
     else
-        printf "${NC}[ ${RED}error ${NC}] Updating alias for ${name} to {$alias}\n"
+        printf "${NC}[ ${RED}error ${NC}] Updating alias for ${name} to ${alias}\n"
+    fi
+
+    if echo ${virtual} >> /etc/postfix/virtual; then
+        printf "${NC}[ ${GREEN}ok ${NC}] Virtual user ${name} mapped by -> ${virtual}\n"
+    else
+        printf "${NC}[ ${RED}error ${NC}] Virtual user ${name} mapped by -> ${virtual}\n"
     fi
 done
 
+echo "" >> /etc/postfix/aliases
+echo "" >> /etc/postfix/virtual
+
 newaliases
+
+postmap /etc/postfix/virtual
 
 echo -e "\n"
